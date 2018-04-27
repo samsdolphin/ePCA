4cdaa9c9cbb75e42f28c3670947654d51852ce2a
Xiyuan Liu <liuxiyuan95@gmail.com>
2018-04-27 23:16:31 +0800

    update purifyImage.m

--- purifyImage.m
+++ purifyImage.m
@@ -1,22 +1,30 @@
-function purified_img = purifyImage(img,gamma)
+function purified_img = purifyImage(set)
 
-[m,n,~] = size(img);
-Y = img(:,:,1);
-Y_bar = zeros(m,1);
+[h,w,n] = size(set);
+p = h*w;
+gamma = p/n;
+X = zeros(p,n);
+Y = X;
+for i = 1:n
+    Y(:,i) = reshape(double(set(:,:,i)),[p,1]);
+end
 
+Y_bar = zeros(p,1);
 for i=1:n
     Y_bar = Y_bar + Y(:,i);
 end
 Y_bar = Y_bar/n;
 
-S = zeros(m,m);
+S = zeros(p,p);
 for i=1:n
     S = S + (Y(:,i)-Y_bar)*(Y(:,i)-Y_bar)';
 end
 S = S/n;
-Sw = diag(sqrt(Y_bar))*S*diag(sqrt(Y_bar));
+%Sd = S - diag(Y_bar);
+Sw = diag(sqrt(Y_bar))\S*inv(diag(sqrt(Y_bar)));
 
 [V,D] = eig(Sw);
+D = real(D);
 for i = 1:length(D)
     if D(i,i)>(1+sqrt(gamma))^2
         D(i,i) = (D(i,i)+1-gamma+sqrt((D(i,i)+1-gamma).^2-4.*D(i,i)))/2;
@@ -25,3 +33,12 @@ for i = 1:length(D)
     end
 end
 Sw = V*D*inv(V);
+Sw = diag(sqrt(Y_bar))*Sw*diag(sqrt(Y_bar));
+
+for i = 1:n
+    X(:,i) = Sw*(diag(Y_bar)+Sw)\Y(:,i) + diag(Y_bar)*(diag(Y_bar)+Sw)\Y_bar;
+end
+
+purified_img = X;
+
+end
